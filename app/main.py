ad8e0510ec9a09744b2c6c1ef752a9818bb146d5
MhmdRyhn <rayhan.cse12@gmail.com>
2020-04-03 02:29:53 +0600

    add error and exception handler to the app

--- app/main.py
+++ app/main.py
@@ -12,10 +12,33 @@ def _create_app():
         blueprint_obj = getattr(module, 'blueprint')
         url_prefix = getattr(module, 'url_prefix')
         app.register_blueprint(blueprint_obj, url_prefix=url_prefix)
+
+    @app.errorhandler(Exception)
+    def handle_exception(exception):
+        print(dir(exception))
+        return flask.jsonify(error_code=500, error_message=str(exception)), 500
+
+    @app.errorhandler(404)
+    def not_found_error(error):
+        return flask.jsonify(error_code=error.code, error_message=str(error)), error.code
+
+    @app.errorhandler(400)
+    @app.errorhandler(422)
+    def handle_error(error):
+        headers = error.data.get("headers", None)
+        messages = error.data.get("messages", ["Invalid request."])
+        if headers:
+            return flask.jsonify({'error_code': error.code, "error_message": messages}), error.code, headers
+        return flask.jsonify({'error_code': error.code, "error_message": messages}), error.code
+
+    @app.errorhandler(500)
+    def not_found_error(error):
+        return flask.jsonify(error_code=error.code, error_message=str(error)), error.code
+
     return app
 
 
-def _debug(host='localhost', port=5000, debug=True):
+def _debug(host='localhost', port=5050, debug=True):
     flask_app = _create_app()
     flask_app.run(host=host, port=port, debug=debug)
 
