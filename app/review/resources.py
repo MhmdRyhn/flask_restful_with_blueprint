51be80b71461d5b08d409fc3df5ced1a62b45267
MhmdRyhn <rayhan.cse12@gmail.com>
2020-04-05 21:02:40 +0600

    add get method to review API

--- app/review/resources.py
+++ app/review/resources.py
@@ -1,22 +1,39 @@
-import uuid
-
 import flask_restful
-from webargs.flaskparser import use_args
+import webargs
+from webargs.flaskparser import use_args, use_kwargs
 
 from app.review.blueprint import API
+from app.review.models import ReviewModel
 from app.review.schemas import ReviewSchema
 
 
-@API.resource('/books/<string:review_id>', strict_slashes=False, methods=['GET', 'PUT', 'DELETE'],
+@API.resource('/books/<string:product_id>', strict_slashes=False, methods=['GET', 'PUT', 'DELETE'],
               endpoint='book-review')
 @API.resource('/books', strict_slashes=False, methods=['POST'], endpoint='add-book-review')
 class ReviewResource(flask_restful.Resource):
     serializer = ReviewSchema
+    model = ReviewModel
 
     @use_args(ReviewSchema)
     def post(self, args):
         data = dict(
-            review_id=str(uuid.uuid4()),
-            review_text=args['review_text']
+            product_id=str(args['product_id']),
+            user_id=str(args['user_id']),
+            review_text=str(args['review_text'])
         )
+
+        review_obj = self.model.dict2model(data)
+        review_obj.save()
+
         return self.serializer().dump(data), 201
+
+    @use_kwargs({'product_id': webargs.fields.UUID()})
+    def get(self, **kwargs):
+        try:
+            review_obj = self.model.get(hash_key=str(kwargs['product_id']))
+        except self.model.DoesNotExist:
+            response = {
+                'msg': 'resource with product id {} does not exist'.format(kwargs['product_id'])
+            }
+            return response, 404
+        return self.serializer().dump(review_obj), 200
